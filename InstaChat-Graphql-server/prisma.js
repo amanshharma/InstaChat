f3cd137dc861d8d4c3fafe9f339dee5c48fc3ba6
Aman Sharma <amandeep.xml@gmail.com>
2020-01-05 19:22:30 -0500

    code cleaning

--- InstaChat-Graphql-server/prisma.js
+++ InstaChat-Graphql-server/prisma.js
@@ -2,7 +2,7 @@ import { Prisma } from "prisma-binding";
 
 const prisma = new Prisma({
   typeDefs: "src/generated/prisma.graphql",
-  endpoint: "http://192.168.99.100:4466/"
+  endpoint: "http://<IP ADDRESS>:4466/" //Prisma end point
   //secret: process.env.PRISMA_SECRET
 });
 export default prisma;
--- InstaChat-Prisma/generated/prisma-client/index.js
+++ InstaChat-Prisma/generated/prisma-client/index.js
@@ -20,6 +20,6 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `http://192.168.99.100:4466`
+  endpoint: `<IP ADDRESS>:4466`
 });
 exports.prisma = new exports.Prisma();
